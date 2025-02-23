3067fff716fd7131638c4a9b5a3b0522a8b920e8
Glax3max <130317166+Glax3max@users.noreply.github.com>
2025-02-23 15:15:26 +0530

    Update About.tsx

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -8,7 +8,7 @@ export const AboutSection = () => {
     title="A Glimps Into My World" 
     description="Learn more about who I am,what I do, and what inspires me." />
      <Card  className=" md:max-w-md md:p-8 m-auto ">
-        <p className="flex justify-around mt-4 md:mt-6 text-sm  md:text-base">My journey into trading began with a fascination with the markets, a desire for financial independence. Through years of study, overcoming challenges, I've developed a strategy that focuses on profit. I'm passionate about helping others navigate the complexities of the markets.</p>
+        <p className="flex justify-around mt-4 md:mt-6 text-sm  md:text-base">My journey into trading began with a fascination with the markets, a desire for financial independence. Through years of study, overcoming challenges, I&apos;ve developed a strategy that focuses on profit. I&apos;m passionate about helping others navigate the complexities of the markets.</p>
       </Card> 
   </div>);
 };

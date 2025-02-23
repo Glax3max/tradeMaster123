215df9daf270671e3d8520d794620df965a8424d
Glax3max <130317166+Glax3max@users.noreply.github.com>
2025-02-23 15:13:22 +0530

    Update About.tsx

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,4 @@
+import { Card } from "@/components/Card";
 import { SectionHeader } from "@/components/SectionHeader";
 export const AboutSection = () => {
   return (
@@ -6,5 +7,8 @@ export const AboutSection = () => {
     eyebrow="About Me" 
     title="A Glimps Into My World" 
     description="Learn more about who I am,what I do, and what inspires me." />
+     <Card  className=" md:max-w-md md:p-8 m-auto ">
+        <p className="flex justify-around mt-4 md:mt-6 text-sm  md:text-base">My journey into trading began with a fascination with the markets, a desire for financial independence. Through years of study, overcoming challenges, I've developed a strategy that focuses on profit. I'm passionate about helping others navigate the complexities of the markets.</p>
+      </Card> 
   </div>);
 };
